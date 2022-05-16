e7b229067eec9b2bc4222a92007e0e0cfcbf22c5
faddey <mohamedavdoll@gmail.com>
2022-05-17 01:51:34 +0300

    create table locomotive_types

--- trainsB.py
+++ trainsB.py
@@ -1,1 +1,12 @@
-print('Hello my friend')
+import sqlite3
+
+def create_tables():
+    conn = sqlite3.connect('railways.db')
+    conn.execute('''CREATE TABLE locomotive_types
+            (
+            locomotive_type_id INTEGER PRIMARY KEY NOT NULL,
+            type_name TEXT NOT NULL
+            );''')
+    conn.commit()
+    conn.close()  
+
