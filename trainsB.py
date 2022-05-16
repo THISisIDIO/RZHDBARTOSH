2c79849aa855dca94e25c73b81b69347cea6a18c
faddey <mohamedavdoll@gmail.com>
2022-05-17 02:04:16 +0300

    Fill locomotive_tipes

--- trainsB.py
+++ trainsB.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 def create_tables():
-    conn = sqlite3.connect('railways.db')
+    conn = sqlite3.connect('railwaysFB.db')
     conn.execute('''CREATE TABLE locomotive_types
             (
             locomotive_type_id INTEGER PRIMARY KEY NOT NULL,
@@ -10,3 +10,36 @@ def create_tables():
     conn.commit()
     conn.close()  
 
+def fill_locomotive_types():
+    conn = sqlite3.connect('railwaysFB.db')
+    conn.execute('''insert into locomotive_types(type_name) values('паровоз');''')
+    conn.execute('''insert into locomotive_types(type_name) values('электровоз');''')
+    conn.execute('''insert into locomotive_types(type_name) values('скоростной');    ''')
+    conn.commit()
+    conn.close()     
+
+def check_tables():
+    conn = sqlite3.connect('railwaysFB.db')
+    cursor = conn.cursor()
+    cursor.execute('''
+    SELECT name FROM sqlite_master WHERE type='table';''')
+    res = cursor.fetchall()
+    for r in res:
+        print(r)
+    
+def db_init():    
+    create_tables()
+    check_tables()
+    fill_locomotive_types()
+
+from os.path import exists
+if not exists('railwaysFB.db'):
+    db_init()
+#fill_locomotive_types()
+conn = sqlite3.connect('railwaysFB.db')
+cursor = conn.cursor()
+cursor.execute('''
+SELECT * FROM locomotive_types;''')
+res = cursor.fetchall()
+for r in res:
+    print(r)
